32bdcd3325b76cc6090d600ad13647b040614376
Simon <simonblakemore@ymail.com>
2019-04-03 20:26:29 +0100

    Update script.js
    Refactor code to implement game states

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let canvas;
 let ctx;
 let speed = 12;
-let direction = null;
+let direction = 'down';
+let gameState = 'start';
 
 window.onload = function() {
   let info = document.getElementById('info');
@@ -16,13 +17,48 @@ let playerX = playerY = 10;
 let gridSize = 20;
 let totalCells = 20;
 let cherryX = cherryY = 15;
-let velocityX = velocityY = 0;
+let velocityX = 0, velocityY = 1;
 let trail = [];
-let tail = 5;
+let initialTail = 15;
+let tail = initialTail;
 let score = 0;
 let highScore = 0;
+let invulnerable = true;
+let framesPlayed = 0;
 
-function game() {
+
+function drawBackground() {
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0,0, canvas.width, canvas.height);
+} //end drawBackground()
+
+function drawSnake() {
+  //debugger;
+  ctx.fillStyle = 'lime';
+  for (let i = 0; i < trail.length; i++) {
+    ctx.fillRect(trail[i].x * gridSize, trail[i].y * gridSize, gridSize - 2, gridSize - 2);
+    //if (i = tail) playing = true;
+    if (trail[i].x == playerX && trail[i].y == playerY) {
+      score = 0;
+      tail = initialTail;
+      if (invulnerable === false) gameState = 'over';
+    }
+  }
+} // end drawSnake
+
+function drawCherry() {
+  ctx.fillStyle = 'red';
+  ctx.fillRect(cherryX * gridSize,cherryY * gridSize, gridSize - 2, gridSize - 2);
+  info.innerHTML = `Score: ${score} High Score: ${highScore}`;
+} //end drawCherry()
+
+function startGame() {
+  drawBackground();
+} //end startGame()
+
+function playGame() {
+
+
   // Set the direction of player movement
   playerX += velocityX;
   playerY += velocityY;
@@ -34,22 +70,17 @@ function game() {
   if (playerY > totalCells - 1) playerY = 0;
 
   //Draw background
-  ctx.fillStyle = 'black';
-  ctx.fillRect(0,0, canvas.width, canvas.height);
+  drawBackground();
 
   //Draw snake
-  ctx.fillStyle = 'lime';
-  for (let i = 0; i < trail.length; i++) {
-    ctx.fillRect(trail[i].x * gridSize, trail[i].y * gridSize, gridSize - 2, gridSize - 2);
-    if (trail[i].x == playerX && trail[i].y == playerY) {
-      score = 0;
-      tail = 5;
-    }
-  }
+  drawSnake();
+
 
   //Update snake position (trail array)
   trail.push({x:playerX, y:playerY});
   while(trail.length > tail) trail.shift();
+
+
 
   //Check for snake eating cherry
   if (cherryX == playerX && cherryY == playerY) {
@@ -61,42 +92,78 @@ function game() {
   }
 
   //Draw cherry
-  ctx.fillStyle = 'red';
-  ctx.fillRect(cherryX * gridSize,cherryY * gridSize, gridSize - 2, gridSize - 2);
-  info.innerHTML = `Score: ${score} High Score: ${highScore}`;
+  drawCherry();
+
+
+
+} //end playGame()
+
+function gameOver() {
+  drawBackground();
+} //end gameOver()
+
+function game() {
+
+
+
+  if (gameState === 'start') startGame();
+  if (gameState === 'play') {
+    if (direction !== null) framesPlayed++;
+    if (framesPlayed > initialTail) invulnerable = false;
+    playGame();
+  }
+  if (gameState === 'over') gameOver();
+
+
 } //end game()
 
 
 //handle key presses
 function keyPush(event) {
-  switch(event.keyCode) {
-    case 37: //Left arrow
-      if (direction !== 'right') {
-        direction = 'left';
-        velocityX = -1;
-        velocityY = 0;
-      }
-      break;
-    case 38: //Up arrow
-      if (direction !== 'down') {
-        direction = 'up'
-        velocityX = 0;
-        velocityY = -1;
-      }
-      break;
-    case 39: //Right arrow
-      if (direction !== 'left') {
-        direction = 'right'
-        velocityX = 1;
-        velocityY = 0;
-      }
-      break;
-    case 40: //Down arrow
-      if (direction !== 'up') {
-        direction = 'down';
-        velocityX = 0;
-        velocityY = 1;
-      }
-      break;
-  } //end switch
+  if (gameState === 'play') {
+    switch(event.keyCode) {
+      case 37: //Left arrow
+        if (direction !== 'right') {
+          direction = 'left';
+          velocityX = -1;
+          velocityY = 0;
+        }
+        break;
+      case 38: //Up arrow
+        if (direction !== 'down') {
+          direction = 'up'
+          velocityX = 0;
+          velocityY = -1;
+        }
+        break;
+      case 39: //Right arrow
+        if (direction !== 'left') {
+          direction = 'right'
+          velocityX = 1;
+          velocityY = 0;
+        }
+        break;
+      case 40: //Down arrow
+        if (direction !== 'up') {
+          direction = 'down';
+          velocityX = 0;
+          velocityY = 1;
+        }
+        break;
+    } //end switch
+  }
+
+  if (gameState === 'start') {
+    switch(event.keyCode) {
+      case 13: //Enter
+        gameState = 'play';
+    }
+  }
+
+  if (gameState === 'over') {
+    switch(event.keyCode) {
+      case 32: //Space
+        gameState = 'start';
+    }
+  }
 } //end keyPush()
